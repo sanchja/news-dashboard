32c96ee2973c7c233f58fff47697e547668aad40
Jason Sanchez <jasonsanchez@Jasons-Mac-mini.local>
2019-06-03 01:32:39 -0400

    angular update for material version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NewsApiService } from './news-api.service';
+
+// Material Imports
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule, MatCardModule, MatMenuModule,
+  MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule
+} from '@angular/material';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, MatButtonModule, MatCardModule, MatMenuModule,
+    MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [NewsApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
